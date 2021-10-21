a759c7c107cbaf5f274de376387d12b462f1c304
LzGui <lzguilhermecp@gmail.com>
2021-10-21 15:00:04 -0300

    Projeto iniciado

---
+++ src/main/java/com/udemy/curso_sb/CursoSbApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.curso_sb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoSbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoSbApplication.class, args);
+	}
+
+}
