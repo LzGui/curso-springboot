2129a586d3a632f58268432f7777d00f14372dad
LzGui <lzguilhermecp@gmail.com>
2021-10-25 18:15:49 -0300

    Criando operação de instanciação

--- src/main/java/com/udemy/curso_sb/CursoSbApplication.java
+++ src/main/java/com/udemy/curso_sb/CursoSbApplication.java
@@ -1,13 +1,32 @@
 package com.udemy.curso_sb;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.udemy.curso_sb.domain.Category;
+import com.udemy.curso_sb.repositories.CategoryRepository;
+
 @SpringBootApplication
-public class CursoSbApplication {
+public class CursoSbApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursoSbApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Category cat1 = new Category(null, "Informática");
+		Category cat2 = new Category(null, "Escritório");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 
 }
