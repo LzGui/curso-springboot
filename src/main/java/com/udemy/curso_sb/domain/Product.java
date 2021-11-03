d165ebc07c63ccedc35fd5d0a12c7c27e1447d25
LzGui <lzguilhermecp@gmail.com>
2021-11-03 09:27:55 -0300

    atualizando

--- src/main/java/com/udemy/curso_sb/domain/Product.java
+++ src/main/java/com/udemy/curso_sb/domain/Product.java
@@ -11,25 +11,27 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
-public class Category implements Serializable{
-
+public class Product implements Serializable{
+	
 	private static final long serialVersionUID = 1L;
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String name;
+	private Double price;
 	
-	private List<Product> products = new ArrayList<>();
+	private List<Category> categories = new ArrayList<>(); // vários produtos
 	
-	public Category() {
+	public Product() {
 		
 	}
-	
-	public Category(Integer id, String name) {
+
+	public Product(Integer id, String name, Double price) {
 		super();
 		this.id = id;
 		this.name = name;
+		this.price = price;
 	}
 
 	public Integer getId() {
@@ -47,13 +49,21 @@ public class Category implements Serializable{
 	public void setName(String name) {
 		this.name = name;
 	}
-	
-	public List<Product> getProducts() {
-		return products;
+
+	public Double getPrice() {
+		return price;
 	}
 
-	public void setProducts(List<Product> products) {
-		this.products = products;
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public List<Category> getCategories() {
+		return categories;
+	}
+
+	public void setCategories(List<Category> categories) {
+		this.categories = categories;
 	}
 
 	@Override
@@ -69,11 +79,9 @@ public class Category implements Serializable{
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Category other = (Category) obj;
+		Product other = (Product) obj;
 		return Objects.equals(id, other.id);
 	}
-
-	
 	
 	
 }
